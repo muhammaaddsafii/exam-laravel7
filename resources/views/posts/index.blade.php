9a1f9d8dedade38e6e209630a55e1ba72c108fcb
Muhammad Safii <45284719+muhammaddsafii@users.noreply.github.com>
2021-08-16 12:25:05 +0700

    create-migration-and-factory

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,24 @@
 @extends('layouts/master')
+
+@section('content')
+<div class="container mt-3">
+    <div class="d-flex justify-content-between">
+        <h4>All Post</h4>
+        <a class="btn btn-outline-primary rounded-pill" href="">Create</a>
+    </div>
+    <div>
+        <div class="card mt-3" style="width: 18rem;">
+            <div class="card-header">
+                Title
+            </div>
+            <div class="card-body">
+                Body
+            </div>
+            <div class="card-footer d-flex justify-content-between">
+                Footer
+                <a class="btn btn-warning rounded-pill" href="">Edit</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
