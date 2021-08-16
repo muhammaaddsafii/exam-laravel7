0d1f140c63f7fd20b42882f7e23079f9771be635
Muhammad Safii <45284719+muhammaddsafii@users.noreply.github.com>
2021-08-16 11:20:18 +0700

    layout-oke

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//route untuk home
+Route::get('/', 'HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//route untuk about
+Route::get('/about', 'AboutController@index');
+
+//route untuk post
+Route::get('/post', 'PostController@index');
+
+//route untuk contact
+Route::get('/contact', 'ContactController@index');
+
+//route untuk login
+Route::get('/login', 'LoginController@index');
